4d34a125b1f24959a4edb38cb1a3eaf41bc65190
HappyHackingNinja <kehao.chen@happyhacking.ninja>
2016-11-15 06:56:25 +0800

    23. Props

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,14 @@
 /**
  * Created by hhnj on 10/29/16.
  */
+import React from 'react';
+
+function VideoList(props) {
+  return (
+    <ul className="col-md-4 list-group">
+      {props.videos.length}
+    </ul>
+  );
+}
+
+export default VideoList;
